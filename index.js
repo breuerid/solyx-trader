2039e275cefa86594ba5cdb98815ad27ce5c94fe
David W. Breuer <devops@breuer.id>
2025-09-13 02:01:29 +0200

    Sessie 0 en 1 uitgevoerd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Solyx-Trader gestart");
+import "dotenv/config";
+import logger from "./src/logger.js";
+
+const { APP_NAME, APP_ENV } = process.env;
+
+logger.info(`${APP_NAME} gestart in ${APP_ENV} mode`);
+
+function healthCheck() {
+  return { status: "ok", timestamp: new Date().toISOString() };
+}
+
+logger.info(`Healthcheck: ${JSON.stringify(healthCheck())}`);
